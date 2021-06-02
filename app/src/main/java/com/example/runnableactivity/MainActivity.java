2354b3946a4fe4802a621a7d34fa328b1a65c842
B-G Seo <bumku1234@gmail.com>
2021-06-02 14:16:15 +0900

    Multi-threading 1

--- app/src/main/java/com/example/runnableactivity/MainActivity.java
+++ app/src/main/java/com/example/runnableactivity/MainActivity.java
@@ -8,8 +8,9 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
     Thread wr;
     boolean running = true;
-    String TAG2 = "THREAD2";
-
+    String TAG2 = "THREAD";
+    String TAG1 = "THREAD";
+    Thread w;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,43 +20,59 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        w = new WorkerThread();
+        running = true;
 
+        w.start();
+        Log.v(TAG1,"Now i am in Onstart");
         wr = new Thread(new Runnable() {
             @Override
             public void run() {
                 int i = 0;
-                for (i = 0; i < 20 && running; i++) {
+                for(i=0;i<20 && running;i++){
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-                    Log.v(TAG2, "Runnable time" + i);
+                    Log.v(TAG2,"Runnable time="+i);
                 }
+
             }
         });
         wr.start();
-        Log.v(TAG2, "Now i am in onStart");
+        Log.v(TAG2,"Now i am in Onstart");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         running = false;
-
-        Log.v(TAG2, "Now i am in onStop");
+        Log.v(TAG1,"Now i am in Onstop");
+        Log.v(TAG2,"Now i am in Onstop");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
-        Log.v(TAG2, "Now i am in onPause");
+        running = false;
+        Log.v(TAG1,"Now i am in Onpause");
+        Log.v(TAG2,"Now i am in Onpause");
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
 
-        Log.v(TAG2, "Now i am in onResume");
+    class WorkerThread extends Thread{
+        @Override
+        public void run() {
+            int i = 0 ;
+            for(i=0;i<20 && running;i++){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log.v(TAG1,"Thread times="+i);
+            }
+        }
     }
 }
